c410b76de0edd03a3b8a35ddb0a2e4cd653cfc79
Tim Clark <timeeeee@gmail.com>
2017-06-19 23:30:55 -0500

    add experiment in comparing character distributions

--- project-euler/59/tim/stats.py
+++ project-euler/59/tim/stats.py
@@ -1,4 +1,5 @@
 from string import printable, ascii_letters as letters, punctuation, digits
+from string import ascii_lowercase as lowercase
 from itertools import cycle
 
 LETTER_FREQUENCIES = {
@@ -29,6 +30,26 @@ LETTER_FREQUENCIES = {
     "y": 0.01974,
     "z": 0.00074}
 
+PLAINTEXT = (
+    "(The Gospel of John, chapter 1) 1 In the beginning the Word already "
+    "existed. He was with God, and he was God. 2 He was in the beginning with "
+    "God. 3 He created everything there is. Nothing exists that he didn't "
+    "make. 4 Life itself was in him, and this life gives light to everyone. 5 "
+    "The light shines through the darkness, and the darkness can never "
+    "extinguish it. 6 God sent John the Baptist 7 to tell everyone about the "
+    "light so that everyone might believe because of his testimony. 8 John "
+    "himself was not the light; he was only a witness to the light. 9 The one "
+    "who is the true light, who gives light to everyone, was going to come "
+    "into the world. 10 But although the world was made through him, the "
+    "world didn't recognize him when he came. 11 Even in his own land and "
+    "among his own people, he was not accepted. 12 But to all who believed "
+    "him and accepted him, he gave the right to become children of God. 13 "
+    "They are reborn! This is not a physical birth resulting from human "
+    "passion or plan, this rebirth comes from God.14 So the Word became human "
+    "and lived here on earth among us. He was full of unfailing love and "
+    "faithfulness. And we have seen his glory, the glory of the only Son of "
+    "the Father.")
+
 
 def cipher(message, key):
     return "".join(chr(ord(c) ^ ord(k)) for c, k in zip(message, cycle(key)))
@@ -58,8 +79,49 @@ def count_printable():
         print("digits: ", sum(ciphertext.count(c) for c in digits))
         print()
 
-    print("printable counts: ", printable_counts)    
+    print("printable counts: ", printable_counts)
+
+
+def compare_distributions():
+    """
+    Compare letter distributions in text to expected distributions in the
+    english language. The difference in distributions will be calculated as the
+    sum of the squares of differences in frequency for each letter.
+
+    It looks like this is a useful measure! The error for the actual plaintext
+    is < .003, and the next smallest error is .0388! At the very least this
+    would be good to eliminate most of the possible characters in the key!
+    """
+    errors = []
+    for key in range(256):
+        text = cipher(PLAINTEXT, chr(key))
+
+        # count characters in plaintext
+        distribution = dict((letter, 0) for letter in lowercase)
+        total_chars = 0
+        for char in text:
+            if char in distribution:
+                distribution[char] += 1
+                total_chars += 1
+
+        # Since some texts may not have any letters
+        if total_chars == 0:
+            continue
+    
+        # sum up the squares of the errors for each letter
+        error = 0
+        for char in lowercase:
+            freq = distribution[char] / float(total_chars)
+            error += (freq - LETTER_FREQUENCIES[char])**2
+    
+        errors.append(error)
+
+    return errors
 
 
 if __name__ == "__main__":
-    count_printable()
+    # count_printable()
+    errors = compare_distributions()
+    print(errors)
+    errors.sort()
+    print(errors)
